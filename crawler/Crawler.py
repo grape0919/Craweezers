c59554c60b16b32e1ef3dacfc7ba399cb54cf8a5
김홍교 <hkk@diquest.com>
2020-09-07 15:57:51 +0900

    add hearder for requests

--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -6,11 +6,14 @@ lastPage = False
 
 news_url_list = []
 
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36'}
+lp = 0
 while not lastPage:
     url = f"https://news.naver.com/main/list.nhn?mode=LS2D&sid2=259&sid1=101&mid=shm&date=20200906&page={page}"
     print("!@#!@# url : ", url)
-    r = requests.get(url)
+    r = requests.get(url,headers=headers)
     print("!@#!@# status_code : " + str(r.status_code))
+
     if r.status_code == 200:
 
         soup = BeautifulSoup(r.text, 'html.parser')
@@ -29,20 +32,27 @@ while not lastPage:
             news_url_list.append(f['href'])
 
         # print("!@#!@# new LIST : ", news_url_list)
-
-        if type(soup.find("div", {'class':'paging'}).find("a", {'class':'next nclicks(fls.page)'})) == type(None) :
-            lastPage = False
-        else :
-            lp = 0
-            for p in soup.find_all("div", {'class':'nclicks(fls.page)'}):
+        temp = soup.find("div", {'class':'paging'}).find("a", {'class':'next nclicks(fls.page)'})
+        print("!@#!@# temp : " , temp)
+        if type(temp) == type(None) :
+            for p in soup.find_all("a", {'class':'nclicks(fls.page)'}):
                 print("!@#!@# p.text:",p.text)
                 if lp < int(p.text):
                     lp = int(p.text)
-
+            
+            print("!@#!@# lp : ", lp)
             if lp == page:
                 lastPage = True
+            else:
+                lastPage = False
+        else :
+            lastPage = False
 
         page+=1
 
-    
-    
+    print("!@#!@# lastPage:", lastPage)
+
+
+for u in news_url_list:
+    r = requests.get(u,headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
