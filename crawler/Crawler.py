c29783c6ab87f79bfe47b9b557fda72a0f00965a
김홍교 <hkk@diquest.com>
2020-09-08 11:08:01 +0900

    end changing from request to selenium

--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -1,6 +1,13 @@
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver as wb
+
+options = wb.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+
+driver = wb.Chrome(executable_path='lib/chromedriver.exe', chrome_options=options)
 
 page = 1
 lastPage = False
@@ -12,59 +19,49 @@ lp = 0
 while not lastPage:
     url = f"https://news.naver.com/main/list.nhn?mode=LS2D&sid2=259&sid1=101&mid=shm&date=20200906&page={page}"
     print("!@#!@# url : ", url)
-    r = requests.get(url,headers=headers)
-    print("!@#!@# status_code : " + str(r.status_code))
+    driver.get(url)
 
-    if r.status_code == 200:
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    news1 = soup.find("ul", {'class':'type06_headline'})
+    for f in news1.find_all("a"):
+        news_url_list.append(f['href'])
 
-        soup = BeautifulSoup(r.text, 'html.parser')
-        # print("!@#!@# result : ",soup.text)
+    news2 = soup.find("ul", {'class':'type06'})
+    for f in news1.find_all("a"):
+        news_url_list.append(f['href'])
 
-        news1 = soup.find("ul", {'class':'type06_headline'})
-        # print("!@#!@# news1", news1)
-        for f in news1.find_all("a"):
-            news_url_list.append(f['href'])
-
-        news2 = soup.find("ul", {'class':'type06'})
-        # print("!@#!@# news2", news2)
-        for f in news1.find_all("a"):
-            news_url_list.append(f['href'])
-
-        # print("!@#!@# new LIST : ", news_url_list)
-        temp = soup.find("div", {'class':'paging'}).find("a", {'class':'next nclicks(fls.page)'})
-        print("!@#!@# temp : " , temp)
-        if type(temp) == type(None) :
-            for p in soup.find_all("a", {'class':'nclicks(fls.page)'}):
-                print("!@#!@# p.text:",p.text)
-                if lp < int(p.text):
-                    lp = int(p.text)
-            
-            print("!@#!@# lp : ", lp)
-            if lp == page:
-                lastPage = True
-            else:
-                lastPage = False
-        else :
+    temp = soup.find("div", {'class':'paging'}).find("a", {'class':'next nclicks(fls.page)'})
+    print("!@#!@# temp : " , temp)
+    if type(temp) == type(None) :
+        for p in soup.find_all("a", {'class':'nclicks(fls.page)'}):
+            print("!@#!@# p.text:",p.text)
+            if lp < int(p.text):
+                lp = int(p.text)
+        
+        print("!@#!@# lp : ", lp)
+        if lp == page:
+            lastPage = True
+        else:
             lastPage = False
+    else :
+        lastPage = False
 
-        page+=1
+    page+=1
 
-    print("!@#!@# lastPage:", lastPage)
 
 
 datas = []
 
 for u in news_url_list:
-    r = requests.get(u,headers=headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    driver.get(u)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     print("error soup : ", soup)
-    title = soup.find("h3", {'class':'tts_head'}).get_test()
-    pub_date = soup.find("span", {'class':'t11'}).get_test()
-    content = soup.find("div", {'id':'articleBodyContents'}).get_test()
+    title = soup.find("h3", {'class':'tts_head'}).get_text().replace("\n"," ")
+    pub_date = soup.find("span", {'class':'t11'}).get_text().replace("\n"," ")
+    content = soup.find("div", {'id':'articleBodyContents'}).get_text().replace("\n"," ")
 
     news_pair = (title, content, pub_date, u)
     datas.append(news_pair)
 
 df = pd.DataFrame(datas)
-df.to_csv("./test.csv",sep="\t",na_rep='NaN', encoding='utf-8')
-# ,columns=['TITLE', 'COTENT', 'PUB_DATE', 'URL'])
+df.to_csv("./test.csv",sep="|",na_rep='NaN', encoding='utf-8')
