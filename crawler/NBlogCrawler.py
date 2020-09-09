99109bb8bc1f9b838acef6ce7a426712863a4146
ghdry2563 <ghdry2563@gmail.com>
2020-09-09 09:33:08 +0900

    blog 크롤링 마무리

--- crawler/NBlogCrawler.py
+++ crawler/NBlogCrawler.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 from selenium import webdriver as wb
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # options = wb.ChromeOptions()
@@ -29,6 +32,7 @@ driver.find_element_by_xpath("/html/body/ui-view/div/main/div/div/section/div[1]
 # driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div[2]/div[1]/div/form[8]/fieldset/div/div[5]/input").click()
 
 
+driver.implicitly_wait(10)
 urls = []
 lastPage = False
 pages = []
@@ -38,19 +42,19 @@ while not lastPage:
     driver.get(f"https://section.blog.naver.com/Search/Post.nhn?pageNo={page}&rangeType=ALL&orderBy=sim&keyword=%EB%A1%B1%EB%B0%98%20-%EC%B4%88%EB%A1%B1%EB%B0%98%20-%ED%94%84%EB%9E%AD%ED%81%B4%EB%A6%B0%20-%EC%B9%B4%EC%98%A4%EB%A1%B1%EB%B0%98")
     
     elements = []
-    time.sleep(1)
     element = driver.find_element_by_class_name("area_list_search")
     elements = element.find_elements_by_class_name("desc_inner")
 
     for e in elements:
         urls.append(e.get_attribute("href"))
 
+    print("!@#!@# urls : ", urls)
     soup = bs(driver.page_source, 'html.parser')
     temp = soup.find("a", {'class':'button_next'})
     if temp == None:
         # TODO : 마지막 페이지 번호 추출
         paginationEle = driver.find_element_by_class_name("pagination")
-        pagiEles = paginationEle.find_element_by_class_name("item")
+        pagiEles = paginationEle.find_elements_by_class_name("item")
         lp = 0 
         for e in pagiEles:
             if lp < int(e.text):
@@ -61,11 +65,23 @@ while not lastPage:
 
     page += 1
 
-print("url length : ", len(urls))
+print("url length : ", len(urls))  # 1475
 
 
 time.sleep(10)
-# while not lastPage:
-#     print("!@#!@# url : ", url)
 
-#     soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+for u in urls:
+    driver.get(u)
+    titleEl = driver.find_element_by_class_name("se-component-content").find_element_by_class_name("pcol1").text
+    if titleEl == None:
+        titleEl = driver.find_element_by_class_name("itemSubjectBoldfont")
+    
+    title = titleEl.text
+    conts = driver.find_element_by_class_name()
+    
+    dateEl = driver.find_element_by_class_name("se_publishDate")
+    if dateEl == None:
+        dateEl = driver.find_element_by_class_name("_postAddDate")
+    pub_date = dateEl.text
+
